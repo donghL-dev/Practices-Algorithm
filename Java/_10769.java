2ffb1d983abddb5c28a5437eb1c6e890b61366b0
dongh9508 <odh621@naver.com>
2018-10-10 13:30:51 +0900

    solve 10769

---
+++ Java/_10769.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.*;
+
+public class _10769 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str = in.readLine();
+        int happy = 0;
+        int sad = 0;
+
+        for(int i=0; i<str.length(); i++) {
+            if(str.charAt(i) == ':' && str.charAt(i+1) == '-' && str.charAt(i+2) == ')')
+                happy++;
+            if(str.charAt(i) == ':' && str.charAt(i+1) == '-' && str.charAt(i+2) == '(')
+                sad++;
+        }
+
+        if(happy == 0 && sad == 0)
+            System.out.println("none");
+        else if(happy > sad)
+            System.out.println("happy");
+        else if(happy < sad)
+            System.out.println("sad");
+        else
+            System.out.println("unsure");
+    }
+}
